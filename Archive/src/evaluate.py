2fb8a2a5403b1f00b46966bc0571650da5ccd9de
JohnWang <chenyangjohn@berkeley.edu>
2025-05-17 00:08:49 -0700

    changed

--- Archive/src/evaluate.py
+++ Archive/src/evaluate.py
@@ -62,7 +62,7 @@ def load_vllm_model(model_path: str = MODEL_NAME_OR_PATH):
         tokenizer = AutoTokenizer.from_pretrained(
             model_path,
             trust_remote_code=True,
-            token="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"
+            token=
         )
         
         # 获取词汇表大小
